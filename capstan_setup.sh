5b2a876728659e3c9a8e0825f190782453fc5aca
secheverria <sebastian.echeverria@gmail.com>
2016-10-06 09:35:57 -0300

    Speech Server: updating casptan setup script.

--- capstan_setup.sh
+++ capstan_setup.sh
@@ -7,3 +7,9 @@ curl https://raw.githubusercontent.com/cloudius-systems/capstan/master/scripts/d
 
 sudo apt-get install gradle
 sudo apt-get install default-jdk
+sudo apt-get install qemu-system
+
+sudo adduser $USER kvm
+
+# You may need to log out and re log in for capstan to be a recognizable command after its installation,
+# and group permissions to work.
