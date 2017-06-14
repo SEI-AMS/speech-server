e50b9ccc8720837f38d2a51bb016b767a66a57ee
sebastian_echeverria <secheverria@miuandes.cl>
2017-06-14 16:25:48 -0400

    Updating repos/references for Sphinx4 library (which was moved) and capstan installation method.

--- capstan_setup.sh
+++ capstan_setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install curl
-curl https://raw.githubusercontent.com/cloudius-systems/capstan/master/scripts/download | bash
-#sudo apt-get install libedit-dev
 
 sudo apt-get install gradle
 sudo apt-get install default-jdk
 sudo apt-get install qemu-system
+sudo apt-get install golang
+
+go get github.com/cloudius-systems/capstan
 
 sudo adduser $USER kvm
 
